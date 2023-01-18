d3dcb98b1142c3907684ee7f951a419f36204afb
Alejandro Valenzuela <tzalejo@gmail.com>
2023-01-18 03:03:40 -0300

    jest - expect - toBe

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,7 +1,15 @@
-test("should first", () => {
-    // Evaluar 
-    
-    if(false){
-        throw new Error('N puede dividir por cero');
-    }
+// Agrupamos las pruebas:
+describe("Pruebas en <Demo Component 2>", () => {
+  test("should first", () => {
+    // Evaluar
+
+    // 1. inicializacion
+    const message1 = "Hola mundo";
+
+    // 2. estimulo
+    const message2 = message1.trim();
+
+    // 3. Observar el comportamiento -- esperado
+    expect(message1).toBe(message2);
+  });
 });
