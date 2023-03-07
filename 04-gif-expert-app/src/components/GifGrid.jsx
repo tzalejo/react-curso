d9a028715ed66d29842f52b3dfb84b8475b8c0f6
alejandro valenzuela <alejandrovalenzuela@MacBook-Air-de-alejandro.local>
2023-03-06 21:35:12 -0300

    agrego el mensaje cargando imagenes

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -7,6 +7,9 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
+      {
+        isLoading && (<h2>Cargando...</h2>) // para que el h2 no exista en el page.
+      }
       <div className="card-grid">
       {
         images.map((image) => (
