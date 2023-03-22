3ac4f75c644e17ceee2ced4c3e942cfc32c2177f
Alejandro Valenzuela <tzalejo@gmail.com>
2023-03-22 19:29:57 -0300

    fix

--- 04-gif-expert-app/src/hooks/useFetchGifs.js
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -2,20 +2,21 @@ import { useEffect, useState } from 'react';
 import {getGifGrid} from '../Helpers/getGifs';
 
 export const useFetchGifs = (category) => {
-  
+
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
+    let prueba222 = 0;
     const getImages = async () => {
       setImages(await getGifGrid(category));
       setIsLoading(false);
     };
-  
+
     useEffect(() => {
+      prueba222 = 3;
       getImages();
     }, []); // [] le indicamos que este hoot se dispare por primera vez.
 
-    
+    prueba222 = 5;
     return {
         images,
         isLoading
