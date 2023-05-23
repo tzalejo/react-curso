d9b26151b755c7e81775aa6fa60e259b3504e7c8
Alejandro Valenzuela <tzalejo@gmail.com>
2023-05-22 21:14:01 -0300

    trabajo con useLayoutEffect

--- 05-hook-app/src/03-examples/Quote.jsx
+++ 05-hook-app/src/03-examples/Quote.jsx
@@ -1,8 +1,23 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 export const Quote = ({ name, episode }) => {
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    const { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({ height, width });
+  }, [episode]);
+
   return (
-    <blockquote className="blockquote text-end">
-      <p className="mb-2 ">{name}</p>
-      <footer className="blockquote-footer">{episode}</footer>
-    </blockquote>
+    <>
+      <blockquote style={{ display: "flex" }} className="blockquote text-end">
+        <p className="mb-2" ref={pRef}>
+          {name}
+        </p>
+        <footer className="blockquote-footer">{episode}</footer>
+      </blockquote>
+      <div>{JSON.stringify(boxSize)}</div>
+    </>
   );
 };
---
+++ 05-hook-app/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,33 @@
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch";
+import { LoadinQuote } from "../03-examples/LoadinQuote";
+import { Quote } from "../03-examples/Quote";
+
+export const Layout = () => {
+  const { counter, increment } = useCounter(1);
+
+  const { data, isLoading, hasError } = useFetch(
+    `https://rickandmortyapi.com/api/episode/${counter}`
+  );
+
+  // si tuviera un retorno en igual a  = [ {..}] para acceder al primer
+  // elemento tuvieramos q hacer [0]
+  // y esto lo podemos arreglar algo como esto:
+  // const { var1, var2 } = !!data && data[0];
+  // console.log(data);
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? (
+        <LoadinQuote />
+      ) : (
+        <Quote name={data.name} episode={data.episode} />
+      )}
+      <button type="button" className="btn btn-primary" onClick={increment}>
+        Next quote
+      </button>
+    </>
+  );
+};
